444bf48df90104ea8c64b2bb1d658246cb98156b
Jade <apathyjade@outlook.com>
2019-04-22 10:42:45 +0800

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -59,3 +59,15 @@ function domReady(fn){
     }
   }
 }
+const parseStyleText = function (cssText) {
+  const res = {}
+  const listDelimiter = /;(?![^(]*\))/g
+  const propertyDelimiter = /:(.+)/
+  cssText.split(listDelimiter).forEach(function (item) {
+    if (item) {
+      var tmp = item.split(propertyDelimiter)
+      tmp.length > 1 && (res[tmp[0].trim()] = tmp[1].trim())
+    }
+  })
+  return res
+}
