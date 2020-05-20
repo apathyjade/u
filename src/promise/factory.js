7e77fcf92b2f457b93e585f8b31c8a10e0109f0a
Jade <apathyjade@outlook.com>
2020-05-20 10:40:37 +0800

    Create factory.js

---
+++ src/promise/factory.js
@@ -0,0 +1,9 @@
+export const createPromise = () => {
+  let resolve = null;
+  let reject = null;
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  return { promise, resolve, reject };
+};
